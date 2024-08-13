c46d5308a1f5f0f59d9c406485c37b55af347574
Baehyunsu20 <zoskek5429@gmail.com>
2024-08-13 20:06:03 +0900

    현수 : 정곤이의 단조증가 완료

--- W5/src/Hyunsu/S6190_정곤이의단조증가하는수.java
+++ W5/src/Hyunsu/S6190_정곤이의단조증가하는수.java
@@ -6,13 +6,14 @@ import java.io.*;
  * 순차적으로 자릿수가 같거나 증가하는 수 : 단조 증가하는 수
  * 주어진 값을 순차적으로 모두 곱해보고 단조 증가하는 수 중 최대를 찾기
  * 
- * 시간초과 뜸
+ * 시간초과 뜸 -> 어떤 방식으로 해결할까. -> 매서드로 분리하고, 단조 증가 수가 없을 경우는 -1
  * 
  */
 
 public class S6190_정곤이의단조증가하는수 {
 //	static List<Integer> list = new ArrayList<>();
-	static int max =0;
+	//단조가 증가 수가 없을 경우에는 -1이다....
+	static int max =-1;
 	static String tmp;
 	
 	public static void main(String[] args) throws FileNotFoundException {
@@ -20,11 +21,7 @@ public class S6190_정곤이의단조증가하는수 {
 		
 		int T = sc.nextInt();
 		for(int t=1; t<=T; t++) {
-			//초기화
-//			int[] list 
-//			list = new ArrayList<>();
-			max =0;
-			
+			max =-1;
 			//입력 받기
 			int n = sc.nextInt();
 			int[] list = new int[n];
@@ -33,45 +30,46 @@ public class S6190_정곤이의단조증가하는수 {
 			}
 			
 			for(int i=0; i<n; i++) {
-				for(int j=1; j<n; j++) {
-					if(i != j) {
-						//둘이 같지 않을 경우에만
+				for(int j=i+1; j<n; j++) {
 						int mul = list[i] * list[j];
-						//단조 증가하는 수인지 확인
-						//valueof : 타입 변형해주는 매서드
-						tmp = String.valueOf(mul);
-						String[] tmp2 = tmp.split("");
-						
-						int[] tmpList = new int[tmp.length()];
-						
-						for(int l=0; l<tmp.length(); l++) {
-							tmpList[l]=Integer.parseInt(tmp2[l]);
-							
-//							System.out.println(Arrays.toString(tmpList));
-						}//한글자씩 쪼개서 배열의 요소로 저장. -> [1, 4]
-						
-						for(int k=1; k<tmpList.length; k++) {
-							if(tmpList[k-1] > tmpList[k]) {
-								break;
-							}
-							else {
-								if(k== tmp.length()-1) {
-									//마지막 인덱스까지 왔으면 단조 증가의 수가 맞음
-									//최댓값 비교
-									if(max <= mul) max=mul;
+						//단조 증가하는 수인지 확인 -> 매서드화 시키는 게 좋을듯
 
-								}
-							}
+						if(!(check(mul))) {
+							//해당 매서드가 true일때 -> 단조가 맞을 때 최댓값 갱신
+							//false일 경우에는 다음 턴으로 넘어가야함
+							
+							continue;
+						}
+						else {
+							//true일 경우.
+							//최댓값 갱신.
+							if(max < mul)max = mul;
 							
 						}
 
 					}
+
 					
-				}
+				}System.out.println("#"+t+" "+max);
 			}
-			System.out.println("#"+t+" "+max);
+			
 		}
+
+	private static boolean check(int mul) {
+		//valueof : 타입 변형해주는 매서드
+		String[] tmp = String.valueOf(mul).split("");
+
+		for(int i=1; i<tmp.length; i++) {
+			int pre = Integer.parseInt(tmp[i-1]);
+			int t = Integer.parseInt(tmp[i]);
+			if(pre >t) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 
 	}
 
-}
+
