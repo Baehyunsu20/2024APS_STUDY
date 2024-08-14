f8f11fc28ae2337a49f809bf48bab0323c339d25
zoskek5429 <zoskek5429@gmail.com>
2024-08-14 14:25:01 +0900

    현수 : 백만장자 + 붕어빵 완료

--- W5/src/Hyunsu/S1859_백만장자프로젝트.java
+++ W5/src/Hyunsu/S1859_백만장자프로젝트.java
@@ -1,5 +1,46 @@
 package Hyunsu;
+import java.util.*;
+import java.io.*;
 
 public class S1859_백만장자프로젝트 {
+	
+	static double sum = 0;
+	static double max = 0;
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner sc = new Scanner(new File("input_백만장자.txt"));
+		
+		int T = sc.nextInt();
+		for(int t=1; t<=10; t++) {
+			
+			int size = sc.nextInt();
+			double[] list = new double[size];
+			for(int i=0; i<size; i++) {
+				list[i] = sc.nextInt();
+			}
+			
+			sum = 0;
+			max = list[size-1];
+			
+			//뒤에서부터
+			for(int k = size-1; k>0; k--) {
+				//최댓값
+//				if(max<list[k])max=list[k];
+				
+				if(max > list[k-1]) {
+					//최댓값보다 작을 때.
+					sum += (max - list[k-1]);
+//					System.out.println("sum : "+sum);
+				}
+				else if(max < list[k-1]) {
+					max = list[k-1];
+				}
+			}
+			
+			System.out.println("#"+t+" " +  Math.round(sum));
+		}
+		
+		
+	}
 
 }
--- W5/src/Hyunsu/S1860_진기의최고급붕어빵.java
+++ W5/src/Hyunsu/S1860_진기의최고급붕어빵.java
@@ -1,5 +1,43 @@
 package Hyunsu;
 
-public class S1860_진기의최고급붕어빵 {
 
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class S1860_진기의최고급붕어빵 {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int T = sc.nextInt();
+ 
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int K = sc.nextInt();
+ 
+            int[] client = new int[N];
+ 
+            for (int i = 0; i < N; i++) {
+                client[i] = sc.nextInt();
+            }
+ 
+            Arrays.sort(client);
+ 
+            String answer = "Possible";
+ 
+            for (int i = 0; i < N; i++) {
+                int time = client[i];
+                // time / M : 지금까지 붕어빵 만드는 사이클 몇 번 돌았는가?
+                // (time / M) * K : 지금까지 만들어진 붕어빵 개수
+                if ((time / M) * K < i + 1) {
+                    answer = "Impossible";
+                }
+            }
+ 
+            System.out.println("#" + test_case + " " + answer);
+ 
+        }
+    }
+ 
 }
