adf2b45513ffc31bdcdc67cda151ab4e7de278d4
Baehyunsu20 <zoskek5429@gmail.com>
2024-08-14 19:35:49 +0900

    수정

--- W5/src/Hyunsu/S1859_백만장자프로젝트.java
+++ W5/src/Hyunsu/S1859_백만장자프로젝트.java
@@ -1,5 +1,6 @@
 package Hyunsu;
 
 public class S1859_백만장자프로젝트 {
+	
 
 }
--- W5/src/Hyunsu/S1974_스도쿠검증.java
+++ W5/src/Hyunsu/S1974_스도쿠검증.java
@@ -1,5 +1,11 @@
 package Hyunsu;
+import java.util.*;
+import java.io.*;
 
 public class S1974_스도쿠검증 {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner sc = new Scanner(new File(""));
+		int T = sc.nextInt();
+	}
 
 }
